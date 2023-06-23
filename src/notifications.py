e4dc2bce29cfef50da41cf2387a9c7896043e586
Karol Podgorski <karol_podgorski@epam.com>
2023-06-23 23:05:23 +0200

    Add notifications module

---
+++ src/notifications.py
@@ -0,0 +1,11 @@
+
+
+class Notification:
+    @classmethod
+    def send_text(cls, message: str, number: str) -> bool:
+        pass
+
+    @classmethod
+    def send_email(cls, message: str, email_address: str) -> bool:
+        pass
+
